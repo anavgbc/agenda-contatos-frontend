d9edfc7a40d863316cf3b707bce1d4b846a4172f
anavgbc <agomesbc@gmail.com>
2023-03-26 18:40:56 -0300

    feat: home page style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,51 +1,34 @@
-import Card from "../../components/Card";
 import Header from "../../components/Header";
-import { Container, ContainerPage, MenuBar } from "./style";
-import { AiOutlineSearch } from "react-icons/ai";
-import { HiMenuAlt1 } from "react-icons/hi";
-import { useContext, useEffect, useState } from "react";
+import { Container, ContainerPage } from "./style";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../providers/AuthContext";
 import { api } from "../../services/api";
 import { HomePageContext } from "../../providers/HomeContext";
-import ModalHome from "../../components/Modal";
+import NavOptions from "../../components/NavOptions";
+import ContactsList from "../../components/ContactsList";
+import ModalDelete from "../../components/ModalDelete";
 
 const Home = () => {
   const { token, userData } = useContext(AuthContext);
-  const { refreshList } = useContext(HomePageContext);
 
-  const [contacts, setContacts] = useState([]);
+  const { refreshList, listContacts, contacts, filterList } =
+    useContext(HomePageContext);
 
   useEffect(() => {
     if (token) {
       api.defaults.headers.common.Authorization = `Bearer ${token}`;
+      listContacts(userData);
     }
-
-    api
-      .get(`/users/${userData.id}`)
-      .then((response) => setContacts(response.data[0].contacts))
-      .catch((err) => console.log(err));
   }, [refreshList]);
 
   return (
     <>
-      <Header />
       <ContainerPage>
+        <ModalDelete />
+        <Header />
         <Container>
-          <MenuBar>
-            <HiMenuAlt1 />
-            Contatos
-            <div>
-              <ModalHome isAdd={true}> Adicionar</ModalHome>
-              <AiOutlineSearch />
-            </div>
-          </MenuBar>
-          {contacts.length > 0 ? (
-            contacts.map((contact) => (
-              <Card user={contact} key={contact.id} id={contact.id} />
-            ))
-          ) : (
-            <div>Ops.. não há nada por aqui.</div>
-          )}
+          <NavOptions />
+          <ContactsList list={filterList.length > 0 ? filterList : contacts} />
         </Container>
       </ContainerPage>
     </>
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export const ContainerPage = styled.div`
   width: 100%;
-  height: 90%;
-  background-color: #ffffff;
+  height: 100vh;
+
+  background-image: url(https://www.teahub.io/photos/full/95-958090_minimalistic-simple-background-trees-white-background-nice-and.jpg);
+  background-repeat: no-repeat;
+
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0 auto;
+  flex-direction: column;
 
   .chakra-modal__body {
     width: 90%;
@@ -20,43 +23,12 @@ export const ContainerPage = styled.div`
 export const Container = styled.div`
   width: 95%;
   height: 90%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 15px;
-  background-color: #e8e6e6;
-  border-radius: 5px;
-
-  @media screen and (min-width: 768px) {
-    justify-content: space-evenly;
-    width: 70%;
-    padding: 40px;
-  }
-  @media screen and (min-width: 1024px) {
-    width: 55%;
-  }
-`;
-
-export const MenuBar = styled.div`
-  width: 90%;
-  height: 90%;
+  background-color: rgba(255, 255, 255, 0.65);
+  backdrop-filter: blur(5px);
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin-top: 15px;
-  align-items: center;
-
-  div {
-    width: 30%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
-  }
-
-  button {
-    background-color: #dad8d8;
-    padding: 5px 12px;
-    border-radius: 5px;
-  }
+  align-items: flex-start;
+  gap: 15px;
+  border-radius: 9px;
 `;
