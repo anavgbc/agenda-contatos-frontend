1d2287843fc4f3a2859f52cb25b63f4a14eb3208
anavgbc <agomesbc@gmail.com>
2023-03-26 18:41:37 -0300

    feat: create contact modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,130 +1,88 @@
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-  Button,
-} from "@chakra-ui/react";
+import { Box, Button, Modal } from "@mui/material";
+import Input from "../Input";
 import { HiOutlineMail } from "react-icons/hi";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { ModalBody, ButtonAdd } from "./style";
 import * as yup from "yup";
 import { BsPerson, BsTelephone } from "react-icons/bs";
-import Input from "../Input";
 import { HomePageContext } from "../../providers/HomeContext";
-import { useContext } from "react";
-// import Button from "../ButtonSubmit";
+import { useContext, useState } from "react";
+import { api } from "../../services/api";
 
-const ModalHome = ({ children, id, isAdd }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { createContact, editContact, setCardId, cardId } =
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "8px",
+};
+
+const ModalHome = ({ id, edit }) => {
+  const { handleClose, open, isEdit, cardId, editContact, createContact } =
     useContext(HomePageContext);
 
   const schema = yup.object().shape({
-    email: yup.string().email("E-mail inválido"),
+    // email: yup.string().email("E-mail inválido"),
   });
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
 
   return (
-    <>
-      <Button
-        type="button"
-        onClick={() => {
-          onOpen();
-          setCardId(id);
-        }}
-      >
-        {children}
-      </Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{children} contato</ModalHeader>
-          <ModalCloseButton />
+    <div>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
           <ModalBody>
-            {isAdd ? (
-              <form onSubmit={handleSubmit(createContact)}>
-                <Input
-                  placeholder={"Nome"}
-                  title="Nome"
-                  type="text"
-                  Icon={BsPerson}
-                  {...register("name")}
-                  error={errors.name?.message}
-                  grayColor
-                />
-                <Input
-                  placeholder={"Email"}
-                  title="Email"
-                  type="text"
-                  Icon={HiOutlineMail}
-                  {...register("email")}
-                  error={errors.email?.message}
-                  grayColor
-                />
-                <Input
-                  placeholder={"Numero"}
-                  title="Numero"
-                  type="text"
-                  Icon={BsTelephone}
-                  {...register("number")}
-                  error={errors.number?.message}
-                  grayColor
-                />
-                <button type="submit">Salvar</button>
-              </form>
-            ) : (
-              <form onSubmit={handleSubmit(editContact)}>
-                <Input
-                  placeholder={"Nome"}
-                  title="Nome"
-                  type="text"
-                  Icon={BsPerson}
-                  {...register("name")}
-                  error={errors.name?.message}
-                  grayColor
-                />
-                <Input
-                  placeholder={"Email"}
-                  title="Email"
-                  type="text"
-                  Icon={HiOutlineMail}
-                  {...register("email")}
-                  error={errors.email?.message}
-                  grayColor
-                />
-                <Input
-                  placeholder={"Numero"}
-                  title="Numero"
-                  type="text"
-                  Icon={BsTelephone}
-                  {...register("number")}
-                  error={errors.number?.message}
-                  grayColor
-                />
-                <button type="submit" onClick={() => onClose()}>
-                  Salvar
-                </button>
-              </form>
-            )}
+            {isEdit ? <h3>Editar contato</h3> : <h3>Criar contato</h3>}
+            <form
+              onSubmit={
+                isEdit ? handleSubmit(editContact) : handleSubmit(createContact)
+              }
+            >
+              <Input
+                placeholder={"Nome"}
+                title="Nome"
+                type="text"
+                Icon={BsPerson}
+                {...register("name")}
+                error={errors.name?.message}
+                grayColor
+              />
+              <Input
+                placeholder={"Email"}
+                title="Email"
+                type="text"
+                Icon={HiOutlineMail}
+                {...register("email")}
+                error={errors.email?.message}
+                grayColor
+              />
+              <Input
+                placeholder={"Numero"}
+                title="Numero"
+                type="text"
+                Icon={BsTelephone}
+                {...register("number")}
+                error={errors.number?.message}
+                grayColor
+              />
+              <ButtonAdd type="submit">Salvar</ButtonAdd>
+            </form>
           </ModalBody>
-
-          <ModalFooter></ModalFooter>
-        </ModalContent>
+        </Box>
       </Modal>
-    </>
+    </div>
   );
 };
 export default ModalHome;
--- src/providers/HomeContext/index.jsx
+++ src/providers/HomeContext/index.jsx
@@ -1,33 +1,70 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../../services/api";
 import { AuthContext } from "../AuthContext";
+import { ModalContext } from "../ModalContext";
+import { useForm } from "react-hook-form";
 
 export const HomePageContext = createContext({});
 
 export const HomePageContextProvider = ({ children }) => {
-  const [cardId, setCardId] = useState({});
-  const [refreshList, setRefreshList] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setIsEdit(false);
+  };
 
-  const navigate = useNavigate();
-  const { token } = useContext(AuthContext);
+  const { reset } = useForm();
+
+  const [cardId, setCardId] = useState({});
+  const [contacts, setContacts] = useState([]);
+  const [filterList, setFilterList] = useState([]);
+  const [refreshList, setRefreshList] = useState(false);
+  const [isFavorites, setIsFavorites] = useState(false);
+
+  const [searchContact, setSearchContact] = useState("");
+
+  const { openDelete, setOpenDelete } = useContext(ModalContext);
 
   const createContact = (data) => {
     api
       .post("/contacts", data)
       .then((response) => {
         setRefreshList(!refreshList);
+        handleClose();
+        reset();
       })
       .catch((_) => console.log(_));
   };
 
   const editContact = (data) => {
+    const finalData = {};
+
+    const keys = Object.keys(data);
+    const values = Object.values(data);
+
+    values.forEach((value, index) => {
+      if (value !== "") {
+        if (keys[index] === "name") {
+          finalData.name = value;
+        } else if (keys[index] === "email") {
+          finalData.email = value;
+        } else {
+          finalData.number = value;
+        }
+      }
+    });
+
     api
-      .patch(`/contacts/${cardId}`, data)
+      .patch(`/contacts/${cardId}`, finalData)
       .then((response) => {
         setRefreshList(!refreshList);
+        handleClose();
+        reset();
       })
-      .catch((_) => console.log(_));
+      .catch((_) => console.error(_));
   };
 
   const deleteContact = (id) => {
@@ -35,9 +72,79 @@ export const HomePageContextProvider = ({ children }) => {
       .delete(`/contacts/${id}`)
       .then((response) => {
         setRefreshList(!refreshList);
+        setOpenDelete(false);
       })
       .catch((_) => console.log(_));
   };
+
+  const favoriteContact = (id, status) => {
+    api
+      .patch(`/contacts/${id}`, {
+        favorite: !status,
+      })
+      .then((response) => {
+        setRefreshList(!refreshList);
+      });
+  };
+
+  const listContacts = (user) => {
+    api
+      .get(`/users/${user.id}`)
+      .then((response) => {
+        const list = response.data[0].contacts;
+        setContacts(list.sort((a, b) => a.name.localeCompare(b.name)));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const listFavorites = () => {
+    api.get("/contacts/favorites").then((response) => {
+      setContacts(response.data);
+    });
+  };
+
+  const searchInput = () => {
+    const result = contacts.filter(
+      (elem) =>
+        elem.name.toLowerCase().includes(searchContact.toLowerCase()) ||
+        elem.number.includes(searchContact)
+    );
+
+    setFilterList(result);
+  };
+
+  const teste = () => {
+    const algo = {
+      name: "Ana",
+      email: "",
+      number: "",
+    };
+
+    const chaves = Object.keys(algo);
+    const values = Object.values(algo);
+
+    const data = {};
+
+    console.log(chaves, values);
+
+    values.forEach((elem, index) => {
+      if (elem !== "") {
+        console.log(chaves[index]);
+        const chave = chaves[index];
+
+        if (chave === "name") {
+          data.name = elem;
+        } else if (chave === "email") {
+          data.email = elem;
+        } else {
+          data.number = elem;
+        }
+        console.log(data);
+      }
+    });
+  };
+
+  teste();
 
   return (
     <HomePageContext.Provider
@@ -48,6 +155,23 @@ export const HomePageContextProvider = ({ children }) => {
         setCardId,
         cardId,
         refreshList,
+        favoriteContact,
+        setContacts,
+        contacts,
+        listFavorites,
+        listContacts,
+        setSearchContact,
+        searchContact,
+        searchInput,
+        filterList,
+        setFilterList,
+        handleOpen,
+        handleClose,
+        open,
+        isEdit,
+        setIsEdit,
+        setIsFavorites,
+        isFavorites,
       }}
     >
       {children}
