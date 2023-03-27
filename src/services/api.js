a9246da276cc67f434f0d4a5a19dabbff6f5b950
anavgbc <agomesbc@gmail.com>
2023-03-27 19:24:29 -0300

    feat: api baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
-  timeout: 5000,
+  baseURL: "https://agenda-contatos-api.onrender.com",
+  // timeout: 5000,
 });
